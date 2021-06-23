2b8f8a7cb427309652dfcc7221757883be16b5fd
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-06-23 16:15:05 -0400

    working on user controller

---
+++ src/main/java/com/adhocsensei/ahssenseiapi/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.adhocsensei.ahssenseiapi.controller;
+
+import com.adhocsensei.ahssenseiapi.dao.UserRepository;
+import com.adhocsensei.ahssenseiapi.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepo;
+
+    @GetMapping("/user")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userRepo.findById(id);
+    }
+
+    @PostMapping("/user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody User user) {
+        return userRepo.save(user);
+    }
+
+    @PutMapping("/user/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void createUser(@PathVariable Long id, @RequestBody User user) {
+        Optional<User> userOptional = userRepo.findById(id);
+        System.out.println(userOptional);
+        if (userOptional.isPresent()) {
+            user.setUserId(id);
+            userRepo.save(user);
+        }
+//        user.setUserId(id);
+//        userRepo.save(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUserById(@PathVariable Long id) {
+        userRepo.deleteById(id);
+    }
+
+}
